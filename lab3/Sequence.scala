24d1387c77262acb488b8bc7f54c81b08262673b
itqop <leonkl32@gmail.com>
2021-11-12 10:18:42 +0300

    lab3dot3

--- lab3/Sequence.scala
+++ lab3/Sequence.scala
@@ -1,4 +1,6 @@
 package example.lab3
+import example.lab3.Maps.User
+
 import scala.annotation.tailrec
 
 /** Напишите свои решения в тестовых функциях.
@@ -13,30 +15,75 @@ import scala.annotation.tailrec
   */
 // Примечание: напишите функции с хвостовой рекурсией
 
-object Sequence {
+object Sequence extends App{
 
   /* a) Найдите последний элемент Seq.
    *    
    */
-  def testLastElement[A](seq: Seq[A]): Option[A] = ???
-
+  def testLastElement[A](seq: Seq[A]): Option[A] = {
+    @tailrec
+    def loop(seq: Seq[A]): Option[A] =
+      seq.length match {
+        case 1 => Some(seq.head)
+        case _ => loop(seq.drop(1))
+      }
+    loop(seq)
+}
+  println("a) " + testLastElement(Seq(User("Leo", 19), User("NeLeo", 12), User("TopGamerInTheWorld", 21), User("Ded", 99))))
   /* b) Объедините две Seqs (то есть Seq(1, 2) и Seq(3, 4) образуют Seq((1, 3), (2, 4))) - если Seq длиннее игнорируйте оставшиеся элементы.
    *    
    */
-  def testZip[A](a: Seq[A], b: Seq[A]): Seq[(A, A)] = ???
-
+  def testZip[A](a: Seq[A], b: Seq[A]): Seq[(A, A)] = {
+    @tailrec
+    def loop(res: Seq[(A, A)], a: Seq[A], b: Seq[A]): Seq[(A, A)] =
+    {
+      if (a.nonEmpty && b.nonEmpty)
+      {
+        loop(res :+ (a.head, b.head), a.drop(1), b.drop(1))
+      }
+      else res
+    }
+    loop(Seq.empty, a, b)
+  }
+  println("b) " + testZip(Seq( User("TopGamerInTheWorld", 21), User("Ded", 99)), Seq(User("Leo", 19), User("NeLeo", 12)) ))
   /* c) Проверьте, выполняется ли условие для всех элементов в Seq.
    *    
    */
-  def testForAll[A](seq: Seq[A])(cond: A => Boolean): Boolean = false
+  def testForAll[A](seq: Seq[A])(cond: A => Boolean): Boolean = seq.forall(cond)
 
   /* d) Проверьте, является ли Seq палиндромом
    *    
    */
-  def testPalindrom[A](seq: Seq[A]): Boolean = false
+  def testPalindrom[A](seq: Seq[A]): Boolean = {
+    @tailrec
+    def loop(seq: Seq[A]): Boolean = {
+      seq match {
+        case Nil => true
+        case head :: tail => if (head == tail.last) (loop(seq.drop(1).dropRight(1))) else false
+        case _ => false
+      }}
 
+      loop(seq)
+  }
+  println("d) " + testPalindrom(Seq(1,2,3,4,5)))
+  println("d) " + testPalindrom(Seq(1,2,3,3,2,1)))
   /* e) Реализуйте flatMap используя foldLeft.
    *    
    */
-  def testFlatMap[A, B](seq: Seq[A])(f: A => Seq[B]): Seq[B] = ???
+
+  def testFlatMap[A, B](seq: Seq[A])(f: A => Seq[B]): Seq[B] =  {
+    def flatMamp(out: Seq[B], in: A): Seq[B] = {
+    @tailrec
+    def loop(output: Seq[B], input: Seq[B]): Seq[B] = input match {
+      case Nil => output
+      case head::tail => loop(output :+ head, tail)
+    }
+    loop(out, f(in))
+  }
+  seq.foldLeft(Seq.empty[B])(flatMamp)
+}
+
+  println("e) " + testFlatMap(Seq(1,2,3,4,5))(i => Seq(i, 2*i)))
+  println("test e) "+ Seq(1,2,3,4,5).flatMap(i => Seq(i, 2*i)))
+
 }
--- lab3/Strings.scala
+++ lab3/Strings.scala
@@ -4,19 +4,20 @@ package example.lab3
   * 
   * https://www.scala-lang.org/api/2.12.3/scala/collection/immutable/StringOps.html
   */
-object Strings {
+object Strings extends App{
 
   /* a) Преобразуйте все символы типа Char в верхний регистр (не используйте заглавные буквы).
    *    
    */
-  def testUppercase(str: String): String = ""
+  def testUppercase(str: String): String = str.toUpperCase
 
   /* b) Вставьте следующие значения в строку:
    *       Hi my name is <name> and I am <age> years old.
    *    
    */
-  def testInterpolations(name: String, age: Int): String = ""
+  def testInterpolations(name: String, age: Int): String = s"Hi my name is $name and I am $age years old."
 
+  println("testInterpolations " + testInterpolations("Leo",19))
   /* c) Добавьте два числа в следующую строку:
    *       Hi,
    *       now follows a quite hard calculation. We try to add:
@@ -27,9 +28,15 @@ object Strings {
    * 
    *   
    */
-  def testComputation(a: Int, b: Int): String = ""
+  def testComputation(a: Int, b: Int): String = s"\nHi,\nnow follows a quite hard calculation. We try to add:\n  a := $a\n  b := $b\n\n  result is ${a + b}\n\n"
 
+  println("testComputation " + testComputation(7,10))
   /* d) Если длина строки равна 2, верните всю строку, иначе верните первые два символа строки.
    */
-  def testTakeTwo(str: String): String = ""
+  def testTakeTwo(str: String): String = str.length match {
+    case 2 => str
+    case _ => str.take(2)
+  }
+  println("testTakeTwo " + testTakeTwo("Leon"))
+  println("testTakeTwo " + testTakeTwo("Le"))
 }
